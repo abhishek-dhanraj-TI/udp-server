470f721510a767e243bd0260a69faab008a0b57b
abhishek-dhanraj-TI <abhishek.dhanraj@trilogy.com>
2022-05-29 12:35:04 +0000

    Initial Commit

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+
+# The server's hostname or IP address
+HOST = "olive-barracuda-tibh00bj.ws.trilogy.devspaces.com"
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+
+print(f"Received {data!r}")
